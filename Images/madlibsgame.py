016992cc46bbd56c26f4e5392b8f97ccf2877312
Its-S1d <86716356+Its-S1d@users.noreply.github.com>
2021-06-30 15:19:30 +0530

    Add files via upload

---
+++ Images/madlibsgame.py
@@ -0,0 +1,7 @@
+character_name = input("Enter your name :")
+character_age = input("Enter a number :")
+color = input("Enter a color :")
+print("My name is " + character_name)
+print('I am ' + character_age + " years old")
+print("My favourite color is " + color)
+
